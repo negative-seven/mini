6befc27ebe89ae413d7cb86349ee853745d3a922
negative-seven <21268649+negative-seven@users.noreply.github.com>
2022-04-16 03:55:03 +0200

    Initial commit

---
+++ build.py
@@ -0,0 +1,24 @@
+import pathlib
+import subprocess
+
+pathlib.Path('out/').mkdir(exist_ok=True)
+
+result_compile = subprocess.run([
+    'g++',
+    '-c',
+    '-o', 'out/main.o',
+    'src/main.cpp',
+], check=True)
+
+subprocess.run([
+    'Crinkler.exe',
+    '/OUT:out/out.exe',
+    '/SUBSYSTEM:CONSOLE',
+    '/ENTRY:WinMain',
+    'out/main.o',
+    'kernel32.lib',
+    'user32.lib',
+    'ucrt.lib',
+    'gdi32.lib',
+    'ntdll.lib',
+], check=True)
